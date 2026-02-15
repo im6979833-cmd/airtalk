1b658e397a139a9939aa50b6b72ac1f180a333ea
im6979833-cmd <im6979833@gmail.com>
2026-02-16 00:29:32 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -42,6 +42,9 @@ io.on("connection", (socket) => {
     socket.on("call-declined", () => {
         socket.broadcast.emit("call-declined");
     });
+    socket.on("call-cancelled", () => {
+    socket.broadcast.emit("call-cancelled");
+});
 
     socket.on("call-ended", () => {
         socket.broadcast.emit("call-ended");
@@ -65,8 +68,6 @@ io.on("connection", (socket) => {
 
 });
 
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-    console.log("Server running on port", PORT);
+server.listen(3000, () => {
+    console.log("Server running on port 3000");
 });
